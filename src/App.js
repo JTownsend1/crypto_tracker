3a61e66ef077ede08a3711c0be7c9b4958cf5d60
Jonathon Townsend <jtownsend@outlook.com>
2021-03-07 12:43:20 +0000

    New Netlify update attempt 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { Browser, Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Homepage from './Homepage.js';
 import CoinDetail from './CoinDetail1.js';
 
@@ -9,11 +9,12 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      Hello world!!!!!!
+      Hello HELLLLLLLOOOOOOOOOO
+      <h1>YES!!!!!</h1>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/crypto_tracker' render={() => <Homepage />}/>
-          <Route exact path='/crypto_tracker/Coin-detail/:coinid' render={() => <CoinDetail />}/>
+          <Route exact path='/' render={() => <Homepage />}/>
+          <Route exact path='/Coin-detail/:coinid' render={() => <CoinDetail />}/>
         </Switch>
       </BrowserRouter>
     </div>
